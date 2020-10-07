a8da8e121fdfd20a1b7392e19da8efd77e2ad90a
anyahirota <anyahirota@gmail.com>
2020-10-06 21:41:12 -0700

    Edits to twosum

--- twosum.js
+++ twosum.js
@@ -1,18 +1,3 @@
-function twoNumberSum(array, targetSum) {
-  // Write your code here.
-  const sumEls = [];
-  const remainders = {};
-  for (let i = 0; i < array.length; i++) {
-    let remainder = targetSum - array[i];
-    if (remainders[array[i]]) {
-      sumEls.push(array[i], remainder);
-    } else {
-      remainders[remainder] = true;
-    }
-  }
-  return sumEls;
-}
-
 // O(n) space; 
 // 0(n) time; 
 
@@ -29,4 +14,25 @@ function twoNumberSum(array, targetSum) {
     }
   }
   return sumEls;
+}
+
+
+// O(nlog(n)) time; 
+// O(1) space; 
+
+function twoNumberSum(array, targetSum) {
+    array.sort((a , b) => a-b); 
+    let left = 0; 
+    let right = array.length -1; 
+    while (left < right) {
+        const currentSum = array[left] + array[right]; 
+        if (currentSum === targetSum) {
+            return [array[left], array[right]]; 
+        } else if (currentSum < targetSum) {
+            left++;
+        } else if (currentSum > targetSum) {
+            right--;
+        }
+    }
+    return []; 
 }
