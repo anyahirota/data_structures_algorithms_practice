e2d7f32420a2d849cafe9cdfd81264dbeeba36f3
anyahirota <anyahirota@gmail.com>
2020-10-20 21:49:38 -0700

    Add optimal solution for smallest difference algorithm

--- smallest_difference.js
+++ smallest_difference.js
@@ -1,4 +1,40 @@
+//O(nlog(n) + mlog(m)) time
+//O(1) space
 
+function smallestDifference(arrayOne, arrayTwo) {
+  // Write your code here.
+  arrayOne.sort((a, b) => a - b);
+  arrayTwo.sort((a, b) => a - b);
+  let arrIdx = 0;
+  let arrIdx2 = 0;
+  let lowestSum = null;
+  let currSum = null;
+  let resultArr = [];
+  while (arrIdx < arrayOne.length && arrIdx2 < arrayTwo.length) {
+    let firstNum = arrayOne[arrIdx];
+    let secondNum = arrayTwo[arrIdx2];
+    if (firstNum < secondNum) {
+      currSum = secondNum - firstNum;
+      arrIdx++;
+    } else if (secondNum < firstNum) {
+      currSum = firstNum - secondNum;
+      arrIdx2++;
+    } else {
+        // if equal to each other, sum === 0
+      return [firstNum, secondNum];
+    }
+
+    if (currSum < lowestSum || lowestSum === null) {
+      lowestSum = currSum;
+      resultArr = [firstNum, secondNum];
+    }
+  }
+
+  return resultArr;
+}
+
+// Do not edit the line below.
+exports.smallestDifference = smallestDifference;
 
 // Unoptomized
 // O(n^2) time
