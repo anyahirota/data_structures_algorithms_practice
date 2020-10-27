2b9f181fcbd62c324472e030fcdbc4d232d96170
anyahirota <anyahirota@gmail.com>
2020-10-27 16:46:59 -0700

    Add solution to 3 number sum algorithm

--- three_num_sum.js
+++ three_num_sum.js
@@ -1,5 +1,28 @@
 function threeNumberSum(array, targetSum) {
   // Write your code here.
+  let output = [];
+
+  array.sort((a, b) => a - b);
+
+  for (let i = 0; i < array.length; i++) {
+    let left = i + 1;
+    let right = array.length - 1;
+
+    while (left < right) {
+      let sum = array[i] + array[left] + array[right];
+      if (sum === targetSum) {
+        output.push([array[i], array[left], array[right]]);
+        left++;
+        right--;
+      } else if (sum < targetSum) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return output;
 }
 
 // Do not edit the line below.
